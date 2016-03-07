c78f5d6cc17d53c97c4ee33ea5772f15c5e2b110
shane <haroboy876@gmail.com>
2016-03-07 01:46:26 -0800

    abstract delete form logic

--- api.py
+++ api.py
@@ -41,13 +41,17 @@ class DisplayForm(Form):
     submit = SubmitField("submit")
 
 
+class DeleteForm(Form):
+    _id = TextField('_id', [validators.Required()])
+    submit = SubmitField("submit")
+
+
 @app.route("/", methods=["GET", "POST", "DELETE"])
 def device():
     me = request.args.get("me")
     nearby = request.args.get("nearby")
     proximity_date = datetime.now()
     if request.method == "GET":
-
         result = collection1.find(
             {
                 "$query": {"me": me}, "$orderby": {"proximity_date": 1}
@@ -77,11 +81,12 @@ def device():
             return JSONEncoder().encode(document)
         else:
             _id = JSONEncoder().encode(result.get("_id"))
-            collection1.update(
-                {"_id": ObjectId(_id)}, {
+            updated = collection1.update(
+                {"_id": ObjectId(_id.strip("\""))}, {
                     "$set": {"proximity_date": proximity_date}
                 }
             )
+            return JSONEncoder().encode(updated)
     if request.method == "DELETE":
         result = collection1.find_one({"me": me, "nearby": nearby})
         if result:
@@ -91,13 +96,22 @@ def device():
             return render_template('404.html'), 404
 
 
-@app.route("/form", methods=["GET", "POST", "DELETE"])
-def form_data():
+@app.route("/input-form", methods=["GET", "POST"])
+def input_form():
     form = DataForm(request.form)
     if request.method == 'POST' and form.validate():
         me = request.form['me']
         nearby = request.form['nearby']
         return redirect("/?me={0}&nearby={1}".format(me, nearby), code=307)
+    return render_template('form.html', form=form)
+
+
+@app.route("/delete-form", methods=["GET", "POST"])
+def delete_form():
+    form = DeleteForm(request.form)
+    if request.method == 'POST' and form.validate():
+        _id = request.form['_id']
+        return redirect("/display?_id={0}".format(_id), code=307)
     return render_template('form.html', form=form)
 
 
@@ -109,30 +123,15 @@ def display():
         me = request.form.get('me')
         nearby = request.form.get('nearby')
         _id = request.form.get('_id')
-        return redirect(
-            "/redirect?me={0}&nearby={1}&_id={2}".format(
-                me, nearby, _id
-            ), code=307)
+        if me is not None and nearby is not None:
+            return redirect("/?me={0}&nearby={1}".format(me, nearby), code=307)
+        if _id is not None:
+            result = collection1.find_one({"_id": ObjectId(_id.strip("\""))})
+            collection1.remove({"_id": ObjectId(_id.strip("\""))})
+            return JSONEncoder().encode(result)
+        return render_template('404.html'), 404
     return render_template('display.html', data=data, form=form)
 
 
-@app.route("/redirect", methods=["POST"])
-def alternative_action():
-    if request.method == "POST":
-        me = request.args.get("me")
-        nearby = request.args.get("nearby")
-        if me != "None" and nearby != "None":
-            return redirect("/?me={0}&nearby={1}".format(me, nearby), code=307)
-        result = collection1.find_one({"me": me, "nearby": nearby})
-        _id = request.args.get('_id')
-        print request.args
-        if not result and _id != "None":
-            result = collection1.find_one({"_id": ObjectId(_id)})
-            if result:
-                collection1.remove({"_id": ObjectId(_id)})
-                return JSONEncoder().encode(result)
-    return render_template('404.html'), 404
-
-
 if __name__ == "__main__":
-    app.run("0.0.0.0", debug=True)
+    app.run("0.0.0.0")
